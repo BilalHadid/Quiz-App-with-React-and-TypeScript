6f3c936a21e22f3a40df6ec3a6dbe0d914632107
bilalhadid <hadid0648@gmail.com>
2020-08-16 15:17:25 +0500

    adding Quiz Card but in console

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { getQuizDetail } from "./services/quiz_app";
+import Quizcard from "./Components/Quizcard";
+import { questionType } from "./Types/quiz_types";
 
 function App() {
+  const [quiz, setquiz] = useState<questionType[]>([]);
   useEffect(() => {
     const fetchData = async () => {
       const questions = await getQuizDetail(5, "easy");
       console.log(questions);
+
+      setquiz(questions);
     };
     fetchData();
   }, []);
+  if (!quiz.length) return <h3>Loading ..</h3>;
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Quizcard options={quiz[0].option} question={quiz[0].question} />
     </div>
   );
 }
--- src/services/quiz_app.ts
+++ src/services/quiz_app.ts
@@ -1,18 +1,23 @@
-import { Quiz } from "../Types/quiz_types";
+import { Quiz, questionType } from "../Types/quiz_types";
+
+const suffleArray = (array: string[]) =>
+  [...array].sort(() => Math.random() - 0.5);
 
 export const getQuizDetail = async (
   totalQuestuions: number,
   level: string
-): Promise<Quiz[]> => {
+): Promise<questionType[]> => {
   const res = await fetch(
     `https://opentdb.com/api.php?amount=${totalQuestuions}&diffivulty=${level}&type=multiple`
   );
   let { results } = await res.json();
-  let quizQues = results.map((questionObj: Quiz) => {
+  let quizQues: questionType[] = results.map((questionObj: Quiz) => {
     return {
       question: questionObj.question,
       answer: questionObj.correct_answer,
-      option: questionObj.incorrect_answers.concat(questionObj.correct_answer),
+      option: suffleArray(
+        questionObj.incorrect_answers.concat(questionObj.correct_answer)
+      ),
     };
   });
   return quizQues;
