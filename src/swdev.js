85143d52e93217a00d61c75fc178df233458000a
bilalhadid <hadid0648@gmail.com>
2020-08-25 01:31:26 +0500

    Adding PWA

--- src/swdev.js
+++ src/swdev.js
@@ -1,5 +1,5 @@
 export const swDev = () => {
-  let swDev = `${process.env.PUBLIC_URL}/sw.ts`;
+  let swDev = `${process.env.PUBLIC_URL}/sw.js`;
   navigator.serviceWorker.register(swDev).then((result) => {
     console.log("result", result);
   });
