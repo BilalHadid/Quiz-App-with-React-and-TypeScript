0d58e02f65b6bcd58aadef14009e77889ce748dc
bilalhadid <hadid0648@gmail.com>
2020-08-27 03:36:51 +0500

    Complete quizApp

---
+++ public_/build/precache-manifest.8a046ab6f8546a72562a9d002a3bea02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497e973f560797fa1b6f0c15fad812d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "870a0d5ed9c8254b6a91",
+    "url": "/static/css/main.8123a658.chunk.css"
+  },
+  {
+    "revision": "542e0c9061eb96d5a043",
+    "url": "/static/js/2.cb10361c.chunk.js"
+  },
+  {
+    "revision": "05b857f66ed133b0b0c06eef1a416fca",
+    "url": "/static/js/2.cb10361c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "870a0d5ed9c8254b6a91",
+    "url": "/static/js/main.ee3a2a20.chunk.js"
+  },
+  {
+    "revision": "4af822c69337c4235627",
+    "url": "/static/js/runtime-main.ed5bfbd4.js"
+  },
+  {
+    "revision": "a8cdf8d13054c8858641da23bf21d38c",
+    "url": "/static/media/quiz-logo.a8cdf8d1.png"
+  }
+]);
