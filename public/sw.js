bd9e067876f934ec70009c16b66a8bbda568ab6f
bilalhadid <hadid0648@gmail.com>
2020-08-27 03:40:14 +0500

    Quiz App PWA

--- public/sw.js
+++ public/sw.js
@@ -2,16 +2,21 @@ let cacheData = "QuizPWA";
 
 this.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheData).then((cache) => {
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/static/js/main.chunk.js",
-        "/static/js/1.chunk.js",
-        "/static/js/bundle.js",
-        "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple",
-      ]);
-    })
+    caches
+      .open(cacheData)
+      .then((cache) => {
+        cache.addAll([
+          "/",
+          "/index.html",
+          "/static/js/main.chunk.js",
+          "/static/js/1.chunk.js",
+          "/static/js/bundle.js",
+          "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple",
+        ]);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   );
 });
 
