3c6f4da70108319f92fb72f0d26ce0065f209be2
bilalhadid <hadid0648@gmail.com>
2020-08-28 22:29:27 +0500

    Rm catch

--- public/sw.js
+++ public/sw.js
@@ -2,21 +2,16 @@ let cacheData = "QuizPWA";
 
 this.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(cacheData)
-      .then((cache) => {
-        cache.addAll([
-          "/",
-          "/index.html",
-          "/static/js/main.chunk.js",
-          "/static/js/1.chunk.js",
-          "/static/js/bundle.js",
-          "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple",
-        ]);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/static/js/main.chunk.js",
+        "/static/js/1.chunk.js",
+        "/static/js/bundle.js",
+        "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple",
+      ]);
+    })
   );
 });
 
